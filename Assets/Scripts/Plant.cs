b22d39e98acd151faf630dba0131885f74535797
jsano <jasonlee0607@gmail.com>
2024-01-14 20:54:32 -0800

    Peashooter shoots pea

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plant : MonoBehaviour
+public abstract class Plant : MonoBehaviour
 {
+
+    public int cost;
+    public float recharge;
+    public float damage;
+    public float atkspd;
+    private float period = 0;
+    public int HP;
+    public float range;
+
+    public GameObject projectile;
+    protected Vector3 rightOffset;
+    protected Vector3 topOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rightOffset = new Vector3(1, 0);
+        topOffset = new Vector3(1.15f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        period += Time.deltaTime;
+        if (period >= atkspd)
+        {
+            //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, range);
+            //if (hit)
+            //{ 
+                Attack();
+                period = 0;
+            //}
+        }
+        if (HP <= 0)
+        {
+            Die();
+        }
     }
+
+    protected abstract void Attack();
+
+    protected virtual void ReceiveDamage(int dmg)
+    {
+        HP -= dmg;
+        //StartCoroutine(IFrames());
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
 }
---
+++ Assets/Scripts/StraightProjectile.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightProjectile : MonoBehaviour
+{
+
+    /// <summary> The speed at which this projectile travels </summary>
+    public float speed;
+    //public GameObject dissolve;
+    /// <summary> The amount of damage per projectile </summary>
+    public float dmg;
+    /// <summary> The direction this projectile travels in </summary>
+    [HideInInspector] public Vector3 dir;
+    public float distance;
+    private Vector3 startPos;
+    public bool laneSplash;
+    public bool neighboringLaneSplash;
+
+    private Rigidbody2D RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = Vector3.right;
+        RB = GetComponent<Rigidbody2D>();
+        RB.velocity = dir.normalized * speed;
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(startPos, transform.position) >= distance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary> Called when this projectile hits either an enemy, enemy projectile, or ground, where it disappears in all cases </summary>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (other.GetComponent<Enemy>() != null)
+        //{
+        //    other.GetComponent<Enemy>().receiveDamage(dmg, fromPlayer);
+        //}
+        // I was planning to have a particle system for when the projecile hits something to have a cool shatter effect
+        /*GameObject p0 = Instantiate(dissolve, transform.position, transform.rotation);
+        ParticleSystem.MainModule p = p0.GetComponent<ParticleSystem>().main;
+        p.startColor = GetComponent<SpriteRenderer>().color;
+        p0.GetComponent<ParticleSystemRenderer>().sortingOrder = layer;*/
+        Destroy(gameObject);
+    }
+
+}

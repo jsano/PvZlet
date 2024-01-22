1acb3140f5c8f9a0003bec426df2aa486e21c042
jsano <jasonlee0607@gmail.com>
2024-01-22 00:17:47 -0800

    Screen door zombie and scene reset

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().name); //DEBUG
     }
 
     void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -29,11 +29,11 @@ public class ZombieSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        waves.Add(new List<int>(new int[] { 5 }));
+        waves.Add(new List<int>(new int[] { 6 }));
         waves.Add(new List<int>(new int[] { 0, 1 }));
-        waves.Add(new List<int>(new int[] { 0, 2 }));
-        waves.Add(new List<int>(new int[] { 0, 3 }));
-        waves.Add(new List<int>(new int[] { 4, 2 }));
+        waves.Add(new List<int>(new int[] { 2, 3 }));
+        waves.Add(new List<int>(new int[] { 4, 5 }));
+        waves.Add(new List<int>(new int[] { 6, 6 }));
         StartCoroutine(Spawn());
     }
 

86f6e6c9ac1bb1c913b5a20f0635325adb67afe6
jsano <jasonlee0607@gmail.com>
2024-02-04 17:56:20 -0800

    Lawnmowers

---
+++ Assets/Scripts/Lawnmower.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lawnmower : MonoBehaviour
+{
+
+    private Rigidbody2D RB;
+    private bool launched;
+    public int row;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > Tile.tileObjects[1, 9].transform.position.x + Tile.TILE_DISTANCE.x) Destroy(gameObject);
+        if (launched) 
+        {
+            int c = Mathf.Clamp(Tile.WORLD_TO_COL(transform.position.x), 1, 8);
+            RB.velocity = (Tile.tileObjects[row, c + 1].transform.position - Tile.tileObjects[row, c].transform.position).normalized * 5;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        launched = true;
+        if (collider.GetComponent<Zombie>().row == row) collider.GetComponent<Zombie>().ReceiveDamage(100000, gameObject);
+    }
+
+}

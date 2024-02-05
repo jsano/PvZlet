a50d4cd5665534983dfae01494a460334de823a9
jsano <jasonlee0607@gmail.com>
2024-02-04 19:21:17 -0800

    Giga-football zombie, Jalapeno fix

--- Assets/Scripts/Jalapeno.cs
+++ Assets/Scripts/Jalapeno.cs
@@ -7,6 +7,7 @@ public class Jalapeno : Plant
 
     protected override void Attack(Zombie z)
     {
+        List<GameObject> prev = new List<GameObject>();
         for (int c = 1; c < 10; c++)
         {
             Vector2 diff = Tile.tileObjects[row, Mathf.Min(9, c + 1)].transform.position - Tile.tileObjects[row, c].transform.position;
@@ -15,7 +16,9 @@ public class Jalapeno : Plant
             RaycastHit2D[] all = Physics2D.BoxCastAll(g.transform.position, g.transform.localScale, 0, Vector2.zero, 0, LayerMask.GetMask("Zombie"));
             foreach (RaycastHit2D a in all)
             {
+                if (prev.Contains(a.collider.gameObject)) continue;
                 a.collider.GetComponent<Zombie>().ReceiveDamage(damage, gameObject);
+                prev.Add(a.collider.gameObject);
             }
             GameObject t = Tile.tileObjects[row, c].gridItem;
             if (t != null && t.tag == "Snow") Destroy(t);
